ea5144fa9027ff70e4d8ef5c00d5bd686d3f0491
Uday Kiran <50777104+uk1601@users.noreply.github.com>
2022-11-24 22:44:59 +0530

    Update terraform-block.tf
    versions updated

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
@@ -1,32 +1,32 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.3"
+  required_version = ">= 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 4.40"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.0.1"
+      version = ">= 3.0"
     }    
   }
 }
 
 /*
 Play with Terraform Version
-  required_version = "~> 0.14.3" 
-  required_version = "= 0.14.4"  
-  required_version = ">= 0.13"   
-  required_version = "= 0.13"    
-  required_version = "~> 0.13"   
+  required_version = "~> 1.3.5" 
+  required_version = "= 1.3.5"  
+  required_version = ">= 1.3"   
+  required_version = "= 1.3"    
+  required_version = "~> 1.3"   
 
 
 Play with Provider Version
-      version = "~> 3.0"            
-      version = ">= 3.0.0, < 3.1.0"
-      version = ">= 3.0.0, <= 3.1.0"
-      version = "~> 2.0"
-      version = "~> 3.0"            
+      version = "~> 4.40"            
+      version = ">= 4.40.0, < 4.41.0"
+      version = ">= 4.40.0, <= 4.41.0"
+      version = "~> 3.0"
+      version = "~> 4.40"            
 */
 
